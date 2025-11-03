bcaae218081fbbd37126f26838e062651b1275af
mwikacha <arishamohamad3@gmail.com>
2025-11-03 21:56:46 +0800

    added another print statement

--- src/main/java/my/edu/wix1002/systemoperation/Main.java
+++ src/main/java/my/edu/wix1002/systemoperation/Main.java
@@ -3,5 +3,11 @@ package my.edu.wix1002.systemoperation;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is my first Java program.");
+
+        System.out.println("This is my first Java program.");
+        System.out.println("This is my first Java program.");
+        System.out.println("This is my first Java program.");
+        System.out.println("This is my first Java program.");
     }
 }
